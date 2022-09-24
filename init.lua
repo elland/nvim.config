de129f0cb7276fd53644728e7dad987fc15056e6
Igor Ranieri <igor@elland.me>
2022-09-24 13:20:10 +0200

    Preserve caret position between file close/open

--- init.lua
+++ init.lua
@@ -6,3 +6,8 @@ require 'user.treesitter'
 require 'user.lsp'
 require 'user.completion'
 -- require 'user.evil_lualine'
+
+-- Restore caret position in file
+vim.cmd [[
+  autocmd BufReadPost * if @% !~# '\.git[\/\\]COMMIT_EDITMSG$' && line("'\"") > 1 && line("'\"") <= line("$") | exe "normal! g`\"" | endif 
+]]
